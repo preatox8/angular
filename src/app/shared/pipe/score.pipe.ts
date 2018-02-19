1d6ebf16f2fd8f2ed761ea7b2d6e5ba4abfa2ad6
Jordan F <ferrejordan8@gmail.com>
2018-02-19 10:42:51 +0100

    pipe : personnaliser score

--- src/app/shared/pipe/score.pipe.ts
+++ src/app/shared/pipe/score.pipe.ts
@@ -6,7 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ScorePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return `+ ${value}`;
+    if(value < 0){
+      return `- ${value*-1}`;
+    }else if(value > 0){
+      return `+ ${value}`;
+    }else{
+      return `${value}`;
+    }
   }
 
 }
